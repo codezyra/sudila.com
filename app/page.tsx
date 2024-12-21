d6fbda5da1d7739767a53d8764ed992160ecff05
codezyra <codezyraofficial@gmail.com>
2024-12-21 08:14:48 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
-    <main>
-      <h1>Page</h1>
+    <main className="min-h-screen">
+      <h1 className="flex justify-normal items-center">
+        Welcome to My Portfolio
+      </h1>
     </main>
   );
 }
